3f215991e6ab2259e450778ea9b465eba0abe293
Philip Alldredge <pwa0001@uah.edu>
2017-08-23 12:56:38 -0500

    WIP

--- org.osate.ge/src/org/osate/ge/internal/ui/handlers/LayoutDiagramHandler.java
+++ org.osate.ge/src/org/osate/ge/internal/ui/handlers/LayoutDiagramHandler.java
@@ -5,7 +5,9 @@ import java.util.Objects;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.elk.core.options.CoreOptions;
 import org.eclipse.elk.core.service.DiagramLayoutEngine;
+import org.eclipse.elk.graph.ElkNode;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -17,6 +19,11 @@ public class LayoutDiagramHandler extends AbstractHandler {
 				"unable to retrieve active editor");
 		// TODO: Support progress bar for layout. There is a parameter ins the layout engine. Test.
 		final DiagramLayoutEngine.Parameters params = new DiagramLayoutEngine.Parameters();
+		params.addLayoutRun().configure(ElkNode.class)
+		.setProperty(CoreOptions.ALGORITHM, "org.eclipse.elk.algorithm.layered");
+
+		// .setProperty(LayeredOptions.SPACING_LABEL_NODE, 0.0);
+
 		DiagramLayoutEngine.invokeLayout(editorPart, null, params);
 
 		return null;
