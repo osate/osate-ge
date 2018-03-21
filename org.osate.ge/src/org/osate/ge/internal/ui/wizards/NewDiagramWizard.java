2473f78f0446790c208d5bb7cda8c9d8d1af630c
Philip Alldredge <pwa0001@uah.edu>
2018-03-21 11:05:37 -0500

    WIP

---
+++ org.osate.ge/src/org/osate/ge/internal/ui/wizards/NewDiagramWizard.java
@@ -0,0 +1,26 @@
+package org.osate.ge.internal.ui.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewDiagramWizard extends Wizard implements INewWizard {
+
+	public NewDiagramWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
