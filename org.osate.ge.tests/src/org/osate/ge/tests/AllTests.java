567e9a16b7ceccfc7fb87c0bdd0214de5db3e784
ryan.mcilnay <rm0016@uah.edu>
2018-04-03 14:21:59 -0500

    wip tests

--- org.osate.ge.tests/src/org/osate/ge/tests/AllTests.java
+++ org.osate.ge.tests/src/org/osate/ge/tests/AllTests.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 // TODO fix assert statements
 @RunWith(Suite.class)
 @SuiteClasses({
-		CreateConnectionTest.class
+		// CreateConnectionTest.class
 		// InstantiateAndOpenImplTest.class
 //		CreateNewAADLPackageTest.class, OpenExistingAADLModelTest.class, RenameClassifierTest.class,
 //		OpenAssociatedDiagramTest.class, OpenElementPackageDiagramTest.class, InstantiateAndOpenImplTest.class,
