c042ff2ba5b41752f77e4f9a502c0da5294709e5
ryan <ryan@ryan-Virtual-Machine>
2018-04-13 08:28:44 -0400

    wip tests

--- org.osate.ge.tests/src/org/osate/ge/tests/AgeGefBot.java
+++ org.osate.ge.tests/src/org/osate/ge/tests/AgeGefBot.java
@@ -516,9 +516,10 @@ public class AgeGefBot {
 		printWidgets();
 		clickElementsMouse(editor, elementName);
 		bot.viewByTitle("Properties").setFocus();
-		System.err.println(bot.activeView() + " activeView");
-		System.err.println(bot.activeView().bot().radio("Output") + " output");
-		clickRadio(option);
+		System.err.println(bot.activeView().getTitle() + " activeView");
+		System.err.println(bot.viewByTitle("Properties").getWidget() + " getWidget");
+		System.err.println(bot.activeView().bot().radio(option).click() + " output");
+		// clickRadio(option);
 		editor.setFocus();
 	}
 
