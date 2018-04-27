eaf837d1e67d32b7cb328bbd50381cb982bf088b
developer <developer@RSESC-SW1>
2018-04-27 11:05:52 -0500

    wip

--- org.osate.ge.tests/src/org/osate/ge/tests/AllTests.java
+++ org.osate.ge.tests/src/org/osate/ge/tests/AllTests.java
@@ -7,16 +7,16 @@ import org.junit.runners.Suite.SuiteClasses;
 // TODO fix assert statements
 @RunWith(Suite.class)
 @SuiteClasses({
-	//	OpenAssociatedDiagramTest.class, OpenElementPackageDiagramTest.class
+		OpenAssociatedDiagramTest.class// , OpenElementPackageDiagramTest.class
 		// CreateFlowImplTest.class,
 		// CreateConnectionTest.class
 		// InstantiateAndOpenImplTest.class
-		CreateNewAADLPackageTest.class, OpenExistingAADLModelTest.class, RenameClassifierTest.class,
-		OpenAssociatedDiagramTest.class, OpenElementPackageDiagramTest.class, InstantiateAndOpenImplTest.class,
-		GoToTypeDiagramTest.class, DeletingClassifierTest.class, SelectingDiagramElementContainerTest.class,
-		PerformDiagramLayoutTest.class, CreateConnectionTest.class, SetExtendedClassifierTest.class,
-		FeatureGroupTest.class, ModesTransitionTest.class, MoveShapeUsingKeysTest.class, ElementAppearanceTest.class,
-		CreateFlowImplTest.class// , DemoSystemTest.class
+		// CreateNewAADLPackageTest.class, OpenExistingAADLModelTest.class, RenameClassifierTest.class,
+		// OpenAssociatedDiagramTest.class, OpenElementPackageDiagramTest.class, InstantiateAndOpenImplTest.class,
+		// GoToTypeDiagramTest.class, DeletingClassifierTest.class, SelectingDiagramElementContainerTest.class,
+		// PerformDiagramLayoutTest.class, CreateConnectionTest.class, SetExtendedClassifierTest.class,
+		// FeatureGroupTest.class, ModesTransitionTest.class, MoveShapeUsingKeysTest.class, ElementAppearanceTest.class,
+		// CreateFlowImplTest.class// , DemoSystemTest.class
 })
 public class AllTests {
 	//TODO set up and tear down?
