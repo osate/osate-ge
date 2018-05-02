590a4e241182be2af8bfb0c7138054f27003057e
ryan.mcilnay <rm0016@uah.edu>
2018-05-02 08:41:03 -0500

    ge-tests

--- org.osate.ge/src/org/osate/ge/internal/ui/properties/SetExtendedClassifierPropertySection.java
+++ org.osate.ge/src/org/osate/ge/internal/ui/properties/SetExtendedClassifierPropertySection.java
@@ -36,7 +36,6 @@ import org.osate.aadl2.NamedElement;
 import org.osate.ge.BusinessObjectSelection;
 import org.osate.ge.internal.ui.dialogs.ElementSelectionDialog;
 import org.osate.ge.internal.ui.util.InternalPropertySectionUtil;
-import org.osate.ge.internal.ui.util.UiUtil;
 import org.osate.ge.internal.util.AadlHelper;
 import org.osate.ge.internal.util.ScopedEMFIndexRetrieval;
 import org.osate.ge.ui.properties.PropertySectionUtil;
@@ -72,7 +71,6 @@ public class SetExtendedClassifierPropertySection extends AbstractPropertySectio
 
 		chooseBtn = InternalPropertySectionUtil.createButton(getWidgetFactory(), container, null,
 				setExtendedClassifierListener, "Choose...", SWT.PUSH);
-		chooseBtn.setData(UiUtil.AUTOMATED_SWTBOT_TESTING_KEY, "org.osate.ge.properties.SetExtendedClassifier");
 
 		fd = new FormData();
 		fd.left = new FormAttachment(extendedClassifier, ITabbedPropertyConstants.HSPACE);
